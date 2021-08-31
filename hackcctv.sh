cb9acdb7df683483477ab778d188e315e5bea7d5
MrSpammer27 <82670147+MrSpammer27@users.noreply.github.com>
2021-08-31 17:13:00 +0700

    Add files via upload

---
+++ hackcctv.sh
@@ -0,0 +1,46 @@
+# https://codingan/by/mr-virus-spm.index.html/bused-slebew
+# https://codingan/by/mr-virus-spm.index.html/bused-slebew
+# https://codingan/by/mr-virus-spm.index.html/bused-slebew
+sleep 1
+figlet CCHEK
+echo
+echo $cyan"      (+)"$yellow"  BY MR SPAMMER"$cyan"  (+)"
+echo $green"☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪"    
+echo $red"#"$yellow" WUKONG CYBER CRIME"
+echo $red"#"$yellow" ATACKER BUKANLAH HACKER"
+echo $red"#"$yellow" TIDAK ADA KATA MASTAH"
+echo $green"☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪☪"
+    sleep 1
+    echo
+    echo $green"["$cyan"1"$green"]"$red" MULAI"
+    echo $green"["$cyan"2"$green"]"$red" KELUAR"
+    echo $white
+    sleep 1
+read -p "=> " gruty
+if [ $gruty = 1 ]; then
+sleep 1
+  git clone https://github.com/MrVirusSpm-07/mobile
+  sleep 2
+  echo $purple"Berhasil Menginstall github"$white
+  sleep 2
+  echo
+  echo $purple"Berhasil Menginstall cd"$white
+  cd mobile
+  sleep 2
+clear
+echo $blue
+sleep 1
+figlet CCHEK
+echo
+echo $green"======================================"
+echo $red"("$white"+"$red")"$white" Author : Mr Spammer Spm"$red"          ("$white"+"$red")"
+echo $red"("$white"+"$red")"$white" Team   : WUKONG CYBER CRIME"$red"       ("$white"+"$red")"
+echo $red"("$white"+"$red")"$white" Script : Hack Cctv Publik"$red"      ("$white"+"$red")"
+echo $green"======================================"
+echo $cyan
+sleep 2
+  sh https_mrvirus_mobile.sh
+fi
+if [ $gruty = 2 ]; then
+exit
+fi
